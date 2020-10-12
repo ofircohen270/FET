aeb3889c914abcf883aca3ceeac67322a4658bcd
ofircohen270 <ofircohen270@gmail.com>
2020-10-12 21:46:12 +0300

    contact-section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
+// Modules
 import { NgModule } from '@angular/core';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { YouTubePlayerModule } from '@angular/youtube-player';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Angular Material Modules
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+// Components
 import { AppComponent } from './app.component';
 import { VideoPlayerComponent } from './components/video-player/video-player.component';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 import { LessonCardComponent } from './components/lessons/lesson-card/lesson-card.component';
 import { BadgesSectionComponent } from './components/badges/badges-section/badges-section.component';
 import { LessonsSectionComponent } from './components/lessons/lessons-section/lessons-section.component';
+import { ContactSectionComponent } from './components/contact-section/contact-section.component';
 
 @NgModule({
   declarations: [
@@ -22,16 +30,21 @@ import { LessonsSectionComponent } from './components/lessons/lessons-section/le
     VideoPlayerComponent,
     HeroSectionComponent,
     BadgesSectionComponent,
-    LessonsSectionComponent
+    LessonsSectionComponent,
+    ContactSectionComponent
   ],
   imports: [
+    FormsModule,
     LayoutModule,
     BrowserModule,
     MatIconModule,
+    MatInputModule,
     MatButtonModule,
     AppRoutingModule,
     MatSidenavModule,
+    MatFormFieldModule,
     YouTubePlayerModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule
   ],
   providers: [],
