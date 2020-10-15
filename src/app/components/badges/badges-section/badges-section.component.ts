9ef91abe040366141a048078d9f0422a0e2ce11e
ofircohen270 <ofircohen270@gmail.com>
2020-10-15 17:08:12 +0300

    sticky badges

--- src/app/components/badges/badges-section/badges-section.component.ts
+++ src/app/components/badges/badges-section/badges-section.component.ts
@@ -27,10 +27,14 @@ export class BadgesSectionComponent implements OnInit {
     this.sectionPosition = this.sectionElement.nativeElement.offsetTop
   }
 
+  /**
+   * To keep the badges on the side of the page when the user scrolls down, I am adding the class 'sticky'
+   * which has a fixed position
+   */
   @HostListener('window:scroll', ['$event'])
   handleScroll() {
     const windowScroll = window.pageYOffset;
-    if (windowScroll >= window.innerHeight * 0.85) {
+    if (windowScroll >= window.innerHeight * 0.85 && windowScroll <= 2340) {
       this.sticky = true;
     } else {
       this.sticky = false;
@@ -46,7 +50,7 @@ export class BadgesSectionComponent implements OnInit {
     const numberOfSecondsWatched = Object.keys(lessonVideoData.secondsWatched).length
     const percentageWatched = (numberOfSecondsWatched / lessonVideoData.videoDuration) * 100
 
-    // The icon gets filled from the percentage of 30 - 76, so I am changing the percentage accordingly
-    return 30 + (percentageWatched * (54 / 100))
+    // The icon gets filled from the percentage of 0 - 84, so I am changing the percentage accordingly
+    return  (percentageWatched * (84 / 100))
   }
 }
